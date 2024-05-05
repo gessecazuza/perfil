d7307be220ccf752eb82452704d94950b744fe2e
gessecazuza <gessecazuza@gmail.com>
2024-05-05 15:22:58 -0300

    Deploy AWS

--- scripts/01-os-dependencies.sh
+++ scripts/01-os-dependencies.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-sudo apt update
-sudo apt upgrade
+sudo apt update -y
+sudo apt upgrade -y
 
 # Install Python3 pip
 sudo apt install -y python3-pip
--- userdata.sh
+++ userdata.sh
@@ -19,7 +19,7 @@ cd "/home/ubuntu/$PROJECT_MAIN_DIR_NAME"
 chmod +x scripts/*.sh
 
 # Execute scripts for OS dependencies, Python dependencies, Gunicorn, Nginx, and starting the application
-./scripts/instance_os_dependencies.sh
-./scripts/python_dependencies.sh
-./scripts/gunicorn.sh
-./scripts/start_app.sh
+sudo ./scripts/01-os-dependencies.sh
+sudo ./scripts/02-python.sh
+sudo ./scripts/03-gunicorn.sh
+sudo ./scripts/04-start-app.sh
